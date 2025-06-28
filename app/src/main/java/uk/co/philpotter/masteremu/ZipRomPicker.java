2ccee08d175b2ec0c83ceec939ecddd79b6a32d7
pnvnd <peter@datacrunch.ca>
2025-06-28 15:00:54 -0400

    feat!: immersive full screen while in app

--- app/src/main/java/uk/co/philpotter/masteremu/ZipRomPicker.java
+++ app/src/main/java/uk/co/philpotter/masteremu/ZipRomPicker.java
@@ -297,4 +297,17 @@ public class ZipRomPicker extends Activity {
         setResult(RESULT_OK);
         finish();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
 }
